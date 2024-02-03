c8311034a793461dd620966d8140a93f3831748c
Abhinavaaa <palusaabhinavaram@gmail.com>
2024-02-03 20:54:27 +0530

    init

--- src/_codux/boards/header/header.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Header } from '../../../components/header/new-component/header';
-
-export default createBoard({
-    name: 'Header',
-    Board: () => <Header />,
-    isSnippet: true,
-});
